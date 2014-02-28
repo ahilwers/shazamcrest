39fdab18ea937ad6885ea55c63b6c1d044961640
Achim Hilwers <a.hilwers@gs-networks.de>
2014-02-28 14:44:58 +0100

    Added possibility to register custom type adapters for Json (de-)serialization.

--- src/main/java/com/shazam/shazamcrest/GsonProvider.java
+++ src/main/java/com/shazam/shazamcrest/GsonProvider.java
@@ -9,19 +9,26 @@
  */
 package com.shazam.shazamcrest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSerializer;
+import com.google.gson.TypeAdapter;
 
 /**
  * Provides an instance of {@link Gson}. If any class type has been ignored on the matcher, the {@link Gson} provided
  * will include an {@link ExclusionStrategy} which will skip the serialisation/deserialisation of fields for that type.
  */
 public class GsonProvider {
-	private static Gson GSON = new GsonBuilder().enableComplexMapKeySerialization().setPrettyPrinting().create();
+	//private static Gson GSON = new GsonBuilder().enableComplexMapKeySerialization().setPrettyPrinting().create();
+	
+	private static Map<Class<?>, TypeAdapter<?>> typeAdapters = new HashMap<Class<?>, TypeAdapter<?>>();	
 
 	/**
 	 * Returns a {@link Gson} instance containing {@link ExclusionStrategy} based on the object types to ignore during
@@ -31,21 +38,36 @@ public class GsonProvider {
 	 * @return an instance of {@link Gson}
 	 */
 	public static Gson gson(final List<Class<?>> typesToIgnore) {
-		if (typesToIgnore.size() == 0) {
-			return GsonProvider.GSON;
-		} else {
-			return new GsonBuilder().setPrettyPrinting().enableComplexMapKeySerialization().setExclusionStrategies(new ExclusionStrategy() {
-						@Override
-						public boolean shouldSkipField(FieldAttributes f) {
-							return false;
-						}
-
-						@Override
-						public boolean shouldSkipClass(Class<?> clazz) {
-							return (typesToIgnore.contains(clazz));
-						}
-
-					}).create();
-		}
+		return GsonProvider.getGson(typesToIgnore);
 	}
+	
+	public static void registerTypeAdapter(Class<?> aClass, TypeAdapter<?> adapter) {
+		GsonProvider.typeAdapters.put(aClass, adapter);
+	}
+	
+	private static Gson getGson(final List<Class<?>> typesToIgnore) {
+		GsonBuilder gsonBuilder = new GsonBuilder().enableComplexMapKeySerialization().setPrettyPrinting();
+		for (Map.Entry<Class<?>, TypeAdapter<?>> adapterEntry : GsonProvider.typeAdapters.entrySet()) {
+			gsonBuilder.registerTypeAdapter(adapterEntry.getKey(), adapterEntry.getValue());
+		}
+		if (typesToIgnore.size()>0) {
+			gsonBuilder.setExclusionStrategies(new ExclusionStrategy() {
+				@Override
+				public boolean shouldSkipField(FieldAttributes f) {
+					return false;
+				}
+
+				@Override
+				public boolean shouldSkipClass(Class<?> clazz) {
+					return (typesToIgnore.contains(clazz));
+				}
+			});
+		}
+		return gsonBuilder.create();
+	}
+	
+	
+	
+	
+	
 }
